8d707854aed05c530f114abd512514b65537287a
jules blanc <jules.blanc@unccub.com>
2019-10-09 08:10:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+
+class Dog 
+  
+  attr_accessor :dog 
+  @@all = []
+  
+  def initialize(dog)
+    @dog = dog
+    save 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all 
+    @@all.each {|dogs| puts dogs}
+  end 
+  
+  def save
+    @@all << dog 
+  end 
+  
+  def self.clear_all 
+    @@dog.clear 
+  end 
+  
+  
+end 
